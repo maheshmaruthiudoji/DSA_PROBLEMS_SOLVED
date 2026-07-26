70ccb4517e5739c56e87a0786c87592f98383f7f
Mahesh M UDOJI <maheshudoji8088@gmail.com>
2026-07-26 23:52:50 +0530

    Solve LeetCode 628 - Maximum Product of Three Numbers

---
+++ Leetcode daily/628.py
@@ -0,0 +1,11 @@
+"""
+628. Maximum Product of Three Numbers
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product
+"""
+class Solution:
+    def maximumProduct(self, nums):
+        nums.sort()
+        return max(
+            nums[-1] * nums[-2] * nums[-3],
+            nums[0] * nums[1] * nums[-1]
+        )
